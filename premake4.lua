a175f8916d6fd665b8eed0eddf45f39cb8307b9e
Noel Cower <ncower@gmail.com>
2014-01-10 03:37:17 -0800

    Make C++11 a build option rather than mandatory. For now.
    I have a feeling this won't be optional for very long.

--- premake4.lua
+++ premake4.lua
@@ -1,5 +1,12 @@
+
 solution "snowball"
 configurations { "Debug-Static", "Release-Static", "Debug-Shared", "Release-Shared" }
+
+newoption {
+  trigger       = "c++11",
+  description   = "Enables compilation with c++11 -- uses libc++ instead\n"..
+"                   of libstdc++ as the stdlib"
+}
 
 project "snowball"
 language "C++"
@@ -7,10 +14,12 @@ language "C++"
   files { "src/*.cc", "src/*.hh", "include/*.h" }
 
   includedirs { "include" }
-  buildoptions { "-std=c++11", "-stdlib=libc++" }
   defines { "SZ_BUILDING" }
 
   flags { "ExtraWarnings" }
+
+  configuration "c++11"
+    buildoptions { "-std=c++11", "-stdlib=libc++" }
 
   configuration "Debug-*"
     defines { "DEBUG" }
